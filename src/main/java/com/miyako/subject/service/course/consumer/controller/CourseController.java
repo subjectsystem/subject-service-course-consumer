8729f41191b7926369c92c5bf8ec92caffdc1239
lanxiao wei <weilanxiao@163.com>
2019-08-07 14:51:18 +0800

    初始化项目。。。

---
+++ src/main/java/com/miyako/subject/service/course/consumer/controller/CourseController.java
@@ -0,0 +1,32 @@
+package com.miyako.subject.service.course.consumer.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.miyako.subject.commons.domain.TbCourse;
+import com.miyako.subject.service.course.api.TbCourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * ClassName CourseController
+ * Description //TODO
+ * Author weila
+ * Date 2019-08-07-0007 13:49
+ */
+@Controller
+@RequestMapping(value = "course")
+public class CourseController{
+
+    @Reference(version = "${services.versions.course.v1}")
+    private TbCourseService tbCourseService;
+
+    @GetMapping(value = "list")
+    public String list(Model model) {
+        List<TbCourse> tbCourses = tbCourseService.selectAll();
+        model.addAttribute("tbCourses", tbCourses);
+        return "course/list";
+    }
+}
