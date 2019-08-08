06e0facf0b5aaba1f2fd4d117aae2237940fc408
lanxiao wei <weilanxiao@163.com>
2019-08-08 15:01:43 +0800

    route控制器

---
+++ src/main/java/com/miyako/subject/service/course/consumer/route/RouteCourseServiceImpl.java
@@ -0,0 +1,19 @@
+package com.miyako.subject.service.course.consumer.route;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.miyako.subject.service.course.route.RouteCourseService;
+
+/**
+ * ClassName RouteCourseServiceImpl
+ * Description //TODO
+ * Author weila
+ * Date 2019-08-08-0008 14:45
+ */
+@Service
+public class RouteCourseServiceImpl implements RouteCourseService{
+
+    @Override
+    public void info(){
+
+    }
+}
